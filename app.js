3d4da17c1905c79958a8b45f44b7fc9cd8844e2c
Tran Lan Anh <tranlananh@Trans-MacBook-Air.local>
2026-05-07 16:24:18 +0200

    node app should listen externally

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ const server = http.createServer((req, res) => {
   `);
 });
 
-server.listen(3000, () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log(`Server running on port 3000 — version ${APP_VERSION}`);
 });
